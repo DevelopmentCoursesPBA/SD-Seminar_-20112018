08a2cc82ede11e094de2df554a413f905b6e4fa9
peikba <peik@b-a.dk>
2018-11-25 13:33:45 +0100

    Final

--- PublishApp.ps1
+++ PublishApp.ps1
@@ -1,12 +1,9 @@
 Set-ExecutionPolicy RemoteSigned
 # Import the module for the  cmdLet
 Import-Module 'C:\Program Files\Microsoft Dynamics 365 Business Central\130\Service\NavAdminTool.ps1'
-# Uninstall the app - Delete the data
-Uninstall-NAVApp  -ServerInstance DynamicsNAV130W1 -Name "SD Seminar" # -DoNotSaveData 
-# Remove the app from the database - but keep the tables
-Unpublish-NAVApp -ServerInstance DynamicsNAV130W1 -Name "SD Seminar" 
 # Publish app to database
-Publish-NAVApp -ServerInstance DynamicsNAV130W1 -Path "C:\Users\Peikba\Desktop\D. E. Veloper_SD Seminar_1.0.0.0.app" -SkipVerification
+Publish-NAVApp -ServerInstance DynamicsNAV130W1 -Path "C:\Users\Peikba\Desktop\D. E. Veloper_SD Seminar_1.0.0.0.app" `
+               -SkipVerification
 # Install in tenant
 Install-NAVApp -ServerInstance DynamicsNAV130W1 -Name "SD Seminar"
 # Uninstall the app - Delete the data
@@ -17,3 +14,4 @@ Unpublish-NAVApp -ServerInstance DynamicsNAV130W1 -Name "SD Seminar"
 Sync-NAVTenant -ServerInstance DynamicsNAV130W1
 # Remove the tables if the app is gone
 Sync-NAVApp -ServerInstance DynamicsNAV130W1 -Name "SD Seminar" -Mode Clean
+
